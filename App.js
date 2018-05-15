34377b11f72b4b536dd1dc362b15fd076ce95f95
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-15 16:42:06 +0100

    homme fixes

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Home from './components/Home.js';
-import About from './components/About.js'
+import About from './components/About.js';
+import Search from './components/Search.js'
 export default class App extends React.Component {
   render() {
     return (
---
+++ components/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {view, Text} from 'react-native';
+
+
+export default class Home extends React.Component {
+
+    render() {
+        return (
+            <View>
+                <Text> welcome to the jungle xD check my personal website for
+                    more informations: karimation.com
+                </Text>
+
+            </View>
+        )
+    }
+}
