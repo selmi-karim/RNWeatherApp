d2088d02d5197423e33a46c4bd270c0f087e0ea9
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-16 16:32:57 +0100

    axios

--- components/List.js
+++ components/List.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {View, Text, Button, ActivityIndicator} from 'react-native';
 import globalStyle from './styles/styles.js'
 
 export default class List extends React.Component {
@@ -14,15 +14,29 @@ export default class List extends React.Component {
         super(props)
         console.log('state', this.props.navigation.state)
         this.state = {
-            city: this.props.navigation.state.params.city,
+            city:  'sousse',//this.props.navigation.state.params.city,
             report: null
         }
+        this.fetchWeather()
+    }
+
+    fetchWeather () {
+        axios.get(`https://meteotnapi.herokuapp.com/api?city=${this.state.city}`)
+        .ther((response) => {
+            
+        })
     }
     render() {
-        return (
-            <View style={globalStyle.container}>
-                <Text style={globalStyle.title}> youpiii </Text>
-            </View>
-        )
+        if(this.state.report === null ){
+            return (
+                <ActivityIndicator color={globalStyle.color} size='large'/>
+            )
+        }else {
+            return (
+                <View style={globalStyle.container}>
+                    <Text style={globalStyle.title}> youpiii </Text>
+                </View>
+            )
+        }
     }
 }
