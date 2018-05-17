54f1a42cab307ea4e2a73af41d97ea58163f582f
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-17 11:33:04 +0100

    update

--- components/List.js
+++ components/List.js
@@ -25,8 +25,8 @@ export default class List extends React.Component {
     fetchWeather () {
         axios.get(`https://meteotnapi.herokuapp.com/api?city=${this.state.city}`)
         .then((response) => {
-            console.log(response.data);
-            console.log(typeof response.data)
+            //console.log(response.data.city);
+            //console.log(typeof response.data.error)
             this.setState({report: response.data})            
         })
     }
