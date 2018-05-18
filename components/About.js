81b1e12b4e38c2ffc2b34dffc2c385781af5976d
Selmi11895480 <selmi.kerim@gmail.com>
2018-05-18 10:58:38 +0100

    update

--- components/About.js
+++ components/About.js
@@ -1,17 +1,28 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Image} from 'react-native';
 import globalStyle from './styles/styles.js'
 
 export default class About extends React.Component {
     
 
     render() {
+        let profil = 'https://avatars0.githubusercontent.com/u/21101482?s=400&u=9b552297e46f30be0186a70e74c6137f1c113b62&v=4';
         return (
             <View style={globalStyle.container}>
-                <Text style={globalStyle.title}>I'm a Full Stack Javascript Developer
+                 <Image
+                    style={{width: 200, height: 200}}
+                    source={{uri: profil}}
+                />
+                <Text style={globalStyle.dayTitle}>
+                    Kerim SELMI
+                </Text>
+                <Text style={globalStyle.title}>
+                    I'm a Full Stack Javascript Developer
                     Check my personal website for
                     more informations: karimation.com
                 </Text>
+
+                
             </View>
         )
     }
